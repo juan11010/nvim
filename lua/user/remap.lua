bd3878294a304146c9964cfb60f48875121be6cf
Juan Contreras <juan26_2000@live.com.mx>
2023-04-14 19:55:43 -0600

    window navigation

--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -24,3 +24,17 @@ keymap("n", "N", "Nzzzv")
 
 keymap({"n", "v"}, "<leader>y", [["+y]])
 keymap("n", "<leader>Y", [["+Y]])
+
+-- ctrl + vimkeys to move between windows
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+
