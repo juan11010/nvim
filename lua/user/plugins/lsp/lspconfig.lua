c09839d7ccd0758d9f764bf4d7184fe873b178e3
Juan Contreras <juan26_2000@live.com.mx>
2023-09-30 11:28:03 -0600

    Pyright lsp and remove null-ls

--- lua/user/plugins/lsp/lspconfig.lua
+++ lua/user/plugins/lsp/lspconfig.lua
@@ -12,49 +12,25 @@ return {
 
 		local keymap = vim.keymap -- for conciseness
 
-    -- keybind options
-    local opts = { noremap = true, silent = true }
+		-- keybind options
+		local opts = { noremap = true, silent = true }
 
 		-- enable keybinds only for when lsp server available
 		local on_attach = function(client, bufnr)
-
-      opts.buffer = bufnr
+			opts.buffer = bufnr
 
 			-- set keybinds
-			opts.desc = "Show LSP references"
 			keymap.set("n", "<leader>rr", vim.lsp.buf.references, opts) -- show definition, references
-
-			opts.desc = "Go to declaration"
 			keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
-
-			opts.desc = "Show LSP definitions"
 			keymap.set("n", "gd", vim.lsp.buf.definition, opts) -- show lsp definitions
-
-			opts.desc = "Show LSP implementations"
 			keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
-
-			opts.desc = "Show LSP type definitions"
 			keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
-
-			opts.desc = "See available code actions"
 			keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-
-			opts.desc = "Smart rename"
 			keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
-
-			opts.desc = "Show buffer diagnostics"
 			keymap.set("n", "<leader>gl", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-
-			opts.desc = "Show line diagnostics"
 			keymap.set("n", "gl", vim.diagnostic.open_float, opts) -- show diagnostics for line
-
-			opts.desc = "Go to previous diagnostic"
 			keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-
-			opts.desc = "Go to next diagnostic"
 			keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-
-			opts.desc = "Show documentation for what is under cursor"
 			keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
 			opts.desc = "Restart LSP"
@@ -100,6 +76,12 @@ return {
 
 		-- configure intelephense language server
 		lspconfig["intelephense"].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
+
+		-- configure python server
+		lspconfig["pyright"].setup({
 			capabilities = capabilities,
 			on_attach = on_attach,
 		})
--- lua/user/plugins/lsp/mason.lua
+++ lua/user/plugins/lsp/mason.lua
@@ -1,30 +1,30 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-  },
-  config = function ()
-    -- import mason plugin safely
-    local mason = require("mason")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+	},
+	config = function()
+		-- import mason plugin safely
+		local mason = require("mason")
 
-    -- import mason-lspconfig plugin safely
-    local mason_lspconfig = require("mason-lspconfig")
+		-- import mason-lspconfig plugin safely
+		local mason_lspconfig = require("mason-lspconfig")
 
-    -- enable mason
-    mason.setup()
+		-- enable mason
+		mason.setup()
 
-    mason_lspconfig.setup({
-      -- list of servers for mason to install
-      ensure_installed = {
-        "tsserver",
-        "html",
-        "cssls",
-        "lua_ls",
-        "emmet_language_server",
-      },
-      -- auto-install configured servers (with lspconfig)
-      automatic_installation = true,     -- not the same as ensure_installed
-    })
-
-  end
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"tsserver",
+				"html",
+				"cssls",
+				"lua_ls",
+				"emmet_language_server",
+				"pyright",
+			},
+			-- auto-install configured servers (with lspconfig)
+			automatic_installation = true, -- not the same as ensure_installed
+		})
+	end,
 }
