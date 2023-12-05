2ebdf788cb39f5b608d8d95bfc6c9b2a82aaad2b
juan11010 <juan26_2000@live.com.mx>
2023-12-04 18:55:53 -0600

    ts-context-commentstring updates

--- lua/user/plugins/comment.lua
+++ lua/user/plugins/comment.lua
@@ -1,19 +1,24 @@
 return {
-  "numToStr/Comment.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    "JoosepAlviste/nvim-ts-context-commentstring",
-  },
-  config = function()
-    -- import comment plugin safely
-    local comment = require("Comment")
+    "numToStr/Comment.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        "JoosepAlviste/nvim-ts-context-commentstring",
+    },
+    config = function()
+        -- import comment plugin safely
+        local comment = require("Comment")
 
-    local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+        local ts_context_commentstring = require("ts_context_commentstring")
 
-    -- enable comment
-    comment.setup({
-      -- for commenting tsx and jsx files
-      pre_hook = ts_context_commentstring.create_pre_hook(),
-    })
-  end,
+        ts_context_commentstring.setup({
+            enable_autocmd = false,
+        })
+
+        -- enable comment
+        comment.setup({
+            -- for commenting tsx and jsx files
+            -- pre_hook = ts_context_commentstring.create_pre_hook(),
+            pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+        })
+    end,
 }
--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -50,11 +50,7 @@ return {
                         node_decremental = "<bs>",
                     },
                 },
-                -- enable nvim-ts-context-commentstring plugin for commenting tsx and jsx
-                context_commentstring = {
-                    enable = true,
-                    enable_autocmd = false,
-                },
+
                 -- auto install above language parsers
                 auto_install = true,
             })
