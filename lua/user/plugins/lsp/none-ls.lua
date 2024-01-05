8800d73fc42f8dda2820112bb0aa8830303c449e
Juan Contreras <juan26_2000@live.com.mx>
2024-01-04 20:27:02 -0600

    eslint code actions

--- lua/user/plugins/lsp/none-ls.lua
+++ lua/user/plugins/lsp/none-ls.lua
@@ -4,20 +4,30 @@ return {
         local null_ls = require("null-ls")
         local formatting = null_ls.builtins.formatting
         local diagnostics = null_ls.builtins.diagnostics
+        local code_actions = null_ls.builtins.code_actions
 
         null_ls.setup({
             sources = {
+                -- Formatting
                 formatting.stylua,
                 formatting.prettier,
-                diagnostics.cspell,
+
+                -- Diagnostics
+                diagnostics.codespell,
                 diagnostics.eslint_d.with({
+                    condition = function (utils)
+                        return utils.root_has_file(".eslintrc.js", ".eslintrc.cjs")
+                    end
+                }),
+
+                -- Code actions
+                code_actions.eslint_d.with({
                     condition = function (utils)
                         return utils.root_has_file(".eslintrc.js", ".eslintrc.cjs")
                     end
                 }),
             },
         })
-
         vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, {})
     end,
 }
