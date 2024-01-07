7ee775f1f628c95538c810d96c4d70aa23769e3c
Juan Contreras <juancontreras@Juans-MacBook-Pro.local>
2024-01-06 18:21:12 -0600

    harpoon config changes

--- lua/user/plugins/harpoon.lua
+++ lua/user/plugins/harpoon.lua
@@ -5,24 +5,19 @@ return {
         "plenary",
     },
     config = function()
-        local mark = require("harpoon.mark")
-        local ui = require("harpoon.ui")
-        local keymap = vim.keymap.set
+        local harpoon = require("harpoon")
+        harpoon:setup()
 
-        keymap("n", "<leader>a", mark.add_file)
-        keymap("n", "<leader>h", ui.toggle_quick_menu)
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+        vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        keymap("n", "<leader>q", function()
-            ui.nav_file(1)
-        end)
-        keymap("n", "<leader>w", function()
-            ui.nav_file(2)
-        end)
-        keymap("n", "<leader>e", function()
-            ui.nav_file(3)
-        end)
-        keymap("n", "<leader>r", function()
-            ui.nav_file(4)
-        end)
+        vim.keymap.set("n", "<leader>q", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<leader>w", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<leader>e", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<leader>r", function() harpoon:list():select(4) end)
+
+        -- Toggle previous & next buffers stored within Harpoon list
+        -- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+        -- vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
     end,
 }
