6da98817cfa8a1dd7b81d66b80bf914c8f236b99
Juan Contreras <juan26_2000@live.com.mx>
2025-07-15 12:37:11 -0600

    addtional config to conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,8 +6,8 @@ return {
       lua = { "stylua" },
       javascript = { "prettierd", "prettier", stop_after_first = true },
       typescript = { "prettierd", "prettier", stop_after_first = true },
-      javascriptreact = { "prettierd", "prettier" },
-      typescriptreact = { "prettierd", "prettier" },
+      javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+      typescriptreact = { "prettierd", "prettier", stop_after_first = true },
     },
     format_on_save = {
       timeout_ms = 500,
