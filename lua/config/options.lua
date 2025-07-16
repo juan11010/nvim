976b80bba007d9fcee683de48c4a16ff65908a3d
Juan Contreras <juan26_2000@live.com.mx>
2025-07-16 00:07:52 -0600

    change opt to vim.opt

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,13 +7,11 @@ vim.diagnostic.config({
 })
 
 -- Options
-local opt = vim.opt
-
-opt.completeopt = "menu,menuone,noselect"
-opt.confirm = true
-opt.cursorline = true
-opt.expandtab = true
-opt.fillchars = {
+vim.opt.completeopt = "menu,menuone,noselect"
+vim.opt.confirm = true
+vim.opt.cursorline = true
+vim.opt.expandtab = true
+vim.opt.fillchars = {
   foldopen = "",
   foldclose = "",
   fold = " ",
@@ -21,46 +19,45 @@ opt.fillchars = {
   diff = "╱",
   eob = " ",
 }
-opt.formatexpr = "v:lua.require'conform'.formatexpr()"
-opt.foldlevel = 99
-opt.ignorecase = true
-opt.inccommand = "split"
-opt.jumpoptions = "view"
-opt.laststatus = 3 -- global statusline
-opt.linebreak = true -- Wrap lines at convenient points
-opt.list = true -- Show some invisible characters (tabs...
-opt.number = true -- Print line number
-opt.pumblend = 10 -- Popup blend
-opt.pumheight = 10 -- Maximum number of entries in a popup
-opt.relativenumber = true -- Relative line numbers
-opt.ruler = false -- Disable the default ruler
-opt.scrolloff = 4 -- Lines of context
-opt.shiftround = true -- Round indent
-opt.shiftwidth = 2 -- Size of an indent
-opt.shortmess:append({ W = true, I = true, c = true, C = true })
-opt.showmode = false -- Dont show mode since we have a statusline
-opt.sidescrolloff = 8 -- Columns of context
-opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
-opt.smartcase = true -- Don't ignore case with capitals
-opt.smartindent = true -- Insert indents automatically
-opt.spelllang = { "en", "es" }
--- opt.splitbelow = true -- Put new windows below current
-opt.splitkeep = "screen"
-opt.splitright = true -- Put new windows right of current
-opt.tabstop = 2 -- Number of spaces tabs count for
-opt.termguicolors = true -- True color support
-opt.undofile = true
-opt.undolevels = 10000
-opt.updatetime = 200 -- Save swap file and trigger CursorHold
-opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
-opt.winminwidth = 5 -- Minimum window width
-opt.wrap = false -- Disable line wrap
-opt.smoothscroll = true
-opt.foldmethod = "expr"
-opt.foldtext = ""
-opt.swapfile = false -- turn off swap files
-opt.backup = false -- turn off backup files
-opt.undodir = os.getenv("HOME") .. "/.vim/undodir" -- undo files
-opt.incsearch = true -- incremental search
-opt.isfname:append("@-@")
-opt.backspace = "indent,eol,start" -- backspace
+vim.opt.formatexpr = "v:lua.require'conform'.formatexpr()"
+vim.opt.foldlevel = 99
+vim.opt.ignorecase = true
+vim.opt.inccommand = "split"
+vim.opt.jumpoptions = "view"
+vim.opt.laststatus = 3 -- global statusline
+vim.opt.linebreak = true -- Wrap lines at convenient points
+vim.opt.list = true -- Show some invisible characters (tabs...
+vim.opt.number = true -- Print line number
+vim.opt.pumblend = 10 -- Popup blend
+vim.opt.pumheight = 10 -- Maximum number of entries in a popup
+vim.opt.relativenumber = true -- Relative line numbers
+vim.opt.ruler = false -- Disable the default ruler
+vim.opt.scrolloff = 4 -- Lines of context
+vim.opt.shiftround = true -- Round indent
+vim.opt.shiftwidth = 2 -- Size of an indent
+vim.opt.shortmess:append({ W = true, I = true, c = true, C = true })
+vim.opt.showmode = false -- Dont show mode since we have a statusline
+vim.opt.sidescrolloff = 8 -- Columns of context
+vim.opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
+vim.opt.smartcase = true -- Don't ignore case with capitals
+vim.opt.smartindent = true -- Insert indents automatically
+vim.opt.spelllang = { "en", "es" }
+vim.opt.splitkeep = "screen"
+vim.opt.splitright = true -- Put new windows right of current
+vim.opt.tabstop = 2 -- Number of spaces tabs count for
+vim.opt.termguicolors = true -- True color support
+vim.opt.undofile = true
+vim.opt.undolevels = 10000
+vim.opt.updatetime = 200 -- Save swap file and trigger CursorHold
+vim.opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
+vim.opt.winminwidth = 5 -- Minimum window width
+vim.opt.wrap = false -- Disable line wrap
+vim.opt.smoothscroll = true
+vim.opt.foldmethod = "expr"
+vim.opt.foldtext = ""
+vim.opt.swapfile = false -- turn off swap files
+vim.opt.backup = false -- turn off backup files
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir" -- undo files
+vim.opt.incsearch = true -- incremental search
+vim.opt.isfname:append("@-@")
+vim.opt.backspace = "indent,eol,start" -- backspace
