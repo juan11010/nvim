42db863b0944a14db6d9de5779681e8ef262a86c
Juan Contreras <juan26_2000@live.com.mx>
2025-09-25 19:54:19 -0600

    remove lsp autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,14 +6,3 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.hl.on_yank()
   end,
 })
-
--- keymaps
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(ev)
-    local opts = { buffer = ev.buf }
-    vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
-    vim.keymap.set("n", "<leader>ih", function()
-      vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-    end, opts)
-  end,
-})
