aef4228d3775a0c77ffd2d0a06f50b1ba0732458
Juan Contreras <juan26_2000@live.com.mx>
2025-11-15 10:15:11 -0600

    remove scroll

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -12,7 +12,6 @@ return {
     picker = { enabled = true },
     quickfile = { enabled = true },
     scope = { enabled = true },
-    scroll = { enabled = true },
     statuscolumn = { enabled = true },
     words = { enabled = true },
   },
